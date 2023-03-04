e898da2a1edc6c2e60bc45340c244d463971a4ae
Eros Netto Antunes <erosnetto1002@gmail.com>
2023-03-04 17:00:04 -0300

    Aula105 feita

--- Section 7 Classes/Aula105/index.js
+++ Section 7 Classes/Aula105/index.js
@@ -1,1 +1,36 @@
 // Métodos de instância e estáticos 04/03/23
+
+function teste() {
+    console.log('Este é meu teste');
+}
+
+class ControleRemoto {
+    constructor(tv) {
+        this.tv = tv;
+        this.volume = 0;
+        teste();
+    }
+
+    // Método de instância
+    aumentarVolume() {
+        this.volume += 2;
+    }
+    diminuirVolume() {
+        this.volume -= 2;
+    }
+
+    // Método estático
+    static soma(x, y) {
+        return x + y;
+    }
+}
+const controle1 = new ControleRemoto('LG');
+
+// Método de instância
+controle1.aumentarVolume();
+controle1.aumentarVolume();
+controle1.aumentarVolume();
+console.log(controle1);
+
+// Método estático
+console.log(ControleRemoto.soma(2, 8));
