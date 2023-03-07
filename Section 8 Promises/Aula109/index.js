68af0d3e21ca2f9144ac17beb7d27e8e2bdf1378
Eros Netto Antunes <erosnetto1002@gmail.com>
2023-03-07 17:41:08 -0300

    Aula109

--- Section 8 Promises/Aula109/index.js
+++ Section 8 Promises/Aula109/index.js
@@ -8,10 +8,77 @@ function rand(min, max) {
 
 function esperaAi(msg, tempo) {
     return new Promise((resolve, reject) => {
-        if (typeof msg !== 'string') reject(false);
-
         setTimeout(() => {
-            resolve(msg);
+            if (typeof msg !== 'string') {
+                reject('Cai no ERRO');
+                return;
+            }
+
+            resolve(msg.toUpperCase() + ' - Passei na promise');
+            return;
         }, tempo);   
     });
 }
+
+// Promise.all Promise.race Promise.resolve Promise.reject
+const promises = [
+    esperaAi('Promise 1', rand(1, 5)),
+    esperaAi('Promise 2', rand(1, 5)),
+    esperaAi('Promise 3', rand(1, 5)),
+    // esperaAi(1000, 1000),
+];
+
+// Promise.reject
+function baixaPagina() {
+    const emCache = true;
+
+    if(emCache) {
+        return Promise.reject('Página em cache');
+    } else {
+        return esperaAi('Baixei a página', 3000);
+    }
+}
+
+baixaPagina()
+    .then(dadosPagina => {
+        console.log(dadosPagina);
+    })
+    .catch(e => console.log('ERRO', e));
+
+
+// Promise.resolve
+/*function baixaPagina() {
+    const emCache = true;
+
+    if(emCache) {
+        return Promise.resolve('Página em cache');
+    } else {
+        return esperaAi('Baixei a página', 3000);
+    }
+}
+
+baixaPagina()
+    .then(dadosPagina => {
+        console.log(dadosPagina);
+    })
+    .catch(e => console.log(e));*/
+
+
+// Promise.race
+/*Promise.race(promises)
+.then(valor => {
+    console.log(valor);
+})
+.catch(erro => {
+    console.log(erro);
+});*/
+
+
+// Promise.all
+/*Promise.all(promises)
+    .then(valor => {
+        console.log(valor);
+    })
+    .catch(erro => {
+        console.log(erro);
+    });*/
