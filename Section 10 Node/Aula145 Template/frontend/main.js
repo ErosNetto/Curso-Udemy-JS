653c87451fa04978206eb6b4f761142c7d10027e
Eros Netto Antunes <erosnetto1002@gmail.com>
2023-03-26 17:22:32 -0300

    Section 10 concluída

---
+++ Section 10 Node/Aula145 Template/frontend/main.js
@@ -0,0 +1,4 @@
+import 'core-js/stable';  // Navegadores mais antigos
+import 'regenerator-runtime/runtime';  // Navegadores mais antigos
+
+import './assets/css/style.css';  // CSS
---
+++ Section 10 Node/Aula145 Template/src/controllers/contatoController.js
@@ -0,0 +1,3 @@
+exports.paginaInicial = (req, res) => {
+    res.send('Área de contato.');
+};
