77bcd9811f988d0f0b74f2e7a359857ae5d9af3c
Eros Netto Antunes <erosnetto1002@gmail.com>
2023-04-05 14:00:28 -0300

    Validando o login  no front-end feito

--- Section 11 Projeto Agenda/projetoAgenda/frontend/main.js
+++ Section 11 Projeto Agenda/projetoAgenda/frontend/main.js
@@ -1,4 +1,12 @@
 import 'core-js/stable';  // Navegadores mais antigos
 import 'regenerator-runtime/runtime';  // Navegadores mais antigos
 
-import './assets/css/style.css';  // CSS
+import Login from './modules/Login';
+
+const login = new Login('.form-login');
+const cadastro = new Login('.form-cadastro');
+login.init();
+cadastro.init();
+
+// import './assets/css/style.css';  // CSS
+
