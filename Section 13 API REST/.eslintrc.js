8b457fcf9da24095ba3247a745ac69767c422fbc
Eros Netto <erosnetto1002@gmail.com>
2023-05-07 02:00:32 -0300

    Api-rest começo

---
+++ Section 13 API REST/.eslintrc.js
@@ -0,0 +1,17 @@
+module.exports = {
+  env: {
+    es2021: true,
+    node: true,
+  },
+  extends: 'airbnb-base',
+  overrides: [
+  ],
+  parserOptions: {
+    ecmaVersion: 'latest',
+    sourceType: 'module',
+  },
+  rules: {
+    'no-console': 'off',
+    'class-methods-use-this': 'off',
+  },
+};
