915ebd21033b63b0d45df6cd4294c76791f99a9a
Eros Netto <erosnetto1002@gmail.com>
2023-05-07 13:34:02 -0300

    Sequelize

--- Section 13 API REST/src/controllers/HomeController.js
+++ Section 13 API REST/src/controllers/HomeController.js
@@ -1,8 +1,16 @@
+import Aluno from '../models/Aluno';
+
 class HomeController {
-  index(req, res) {
-    res.json({
-      tudoCerto: true,
+  async index(req, res) {
+    const novoAluno = await Aluno.create({
+      nome: 'Luiz',
+      sobrenome: 'Miranda',
+      email: 'luiz@gmail.com',
+      idade: 43,
+      peso: 84,
+      altura: 1.9,
     });
+    res.json(novoAluno);
   }
 }
 
