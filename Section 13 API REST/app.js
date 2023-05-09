31c87bf28d538db6de8d319125bcd37d4ad217e9
Eros Netto <erosnetto1002@gmail.com>
2023-05-08 23:16:09 -0300

    Upload de arquivos

--- Section 13 API REST/app.js
+++ Section 13 API REST/app.js
@@ -9,6 +9,7 @@ import homeRoutes from './src/routes/homeRoutes';
 import userRoutes from './src/routes/userRoutes';
 import tokenRoutes from './src/routes/tokenRoutes';
 import alunoRoutes from './src/routes/alunoRoutes';
+import fotoRoutes from './src/routes/fotoRoutes';
 
 class App {
   constructor() {
@@ -27,6 +28,7 @@ class App {
     this.app.use('/users/', userRoutes);
     this.app.use('/tokens/', tokenRoutes);
     this.app.use('/alunos/', alunoRoutes);
+    this.app.use('/fotos/', fotoRoutes);
   }
 }
 
---
+++ Section 13 API REST/src/config/multerConfig.js
@@ -0,0 +1,16 @@
+import { match } from 'assert';
+import multer from 'multer';
+import { extname, resolve } from 'path';
+
+const aleatorio = () => Math.floor(Math.random() * 10000 + 10000);
+
+export default {
+  storage: multer.diskStorage({
+    destination: (req, file, cb) => {
+      cb(null, resolve(__dirname, '..', '..', 'uploads'));
+    },
+    filename: (req, file, cb) => {
+      cb(null, `${Date.now()}_${aleatorio()}${extname(file.originalname)}`);
+    },
+  }),
+};
