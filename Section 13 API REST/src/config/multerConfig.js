54c309035e09c001e3330632fdfafaa1f234082d
Eros Netto <erosnetto1002@gmail.com>
2023-05-08 23:40:53 -0300

    Tratando os arquivos

--- Section 13 API REST/src/config/multerConfig.js
+++ Section 13 API REST/src/config/multerConfig.js
@@ -1,10 +1,16 @@
-import { match } from 'assert';
 import multer from 'multer';
 import { extname, resolve } from 'path';
 
 const aleatorio = () => Math.floor(Math.random() * 10000 + 10000);
 
 export default {
+  fileFilter: (req, file, cb) => {
+    if (file.mimetype !== 'image/png' && file.mimetype !== 'image/jpeg') {
+      return cb(new multer.MulterError('Arquivo precisa ser PNG ou JPG'));
+    }
+
+    return cb(null, true);
+  },
   storage: multer.diskStorage({
     destination: (req, file, cb) => {
       cb(null, resolve(__dirname, '..', '..', 'uploads'));
