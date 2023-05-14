a594ef719bccb90d17397e77b693532b8f56e067
Eros Netto <erosnetto1002@gmail.com>
2023-05-14 16:56:40 -0300

    REACT-BASE FEITO

--- Section 15 React - Redux/reactbase/src/store/modules/example/reducer.js
+++ Section 15 React - Redux/reactbase/src/store/modules/example/reducer.js
@@ -1,3 +1,5 @@
+import * as types from '../types';
+
 const initialState = {
   botaoClicado: false,
 };
@@ -5,10 +7,19 @@ const initialState = {
 // eslint-disable-next-line
 export default function (state = initialState, action) {
   switch (action.type) {
-    case 'BOTAO_CLICADO': {
+    case types.BOTAO_CLICADO_SUCCESS: {
+      console.log('Sucesso');
       const newState = { ...state };
       newState.botaoClicado = !newState.botaoClicado;
       return newState;
+    }
+    case types.BOTAO_CLICADO_FAILURE: {
+      console.log('Deu ERRO');
+      return state;
+    }
+    case types.BOTAO_CLICADO_REQUEST: {
+      console.log('Estou fazendo a requisição');
+      return state;
     }
 
     default: {
