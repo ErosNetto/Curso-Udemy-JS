330024b8b321548e56cbdf102207cdb4daf9b12b
Eros Netto <erosnetto1002@gmail.com>
2023-05-16 21:56:43 -0300

    Deslogando usuario

--- Section 15 React - Redux/react-api-rest/src/components/Header/index.js
+++ Section 15 React - Redux/react-api-rest/src/components/Header/index.js
@@ -1,10 +1,30 @@
 import React from 'react';
-import { FaHome, FaSignInAlt, FaUser } from 'react-icons/fa';
+import {
+  FaHome,
+  FaSignInAlt,
+  FaUser,
+  FaCircle,
+  FaPowerOff,
+} from 'react-icons/fa';
 import { Link } from 'react-router-dom';
+import { useSelector, useDispatch } from 'react-redux';
+import { toast } from 'react-toastify';
 
+import * as actions from '../../store/modules/auth/actions';
+import history from '../../services/history';
 import { Nav } from './style';
 
 export default function Header() {
+  const dispatch = useDispatch();
+  const isLoggedIn = useSelector((state) => state.auth.isLoggedIn);
+
+  const handleLogout = (e) => {
+    e.preventDefault();
+    dispatch(actions.loginFailure());
+    toast.warn('Você saiu!');
+    history.push('/');
+  };
+
   return (
     <Nav>
       <Link to="/">
@@ -13,9 +33,18 @@ export default function Header() {
       <Link to="/register">
         <FaUser size={24} />
       </Link>
-      <Link to="/login">
-        <FaSignInAlt size={24} />
-      </Link>
+
+      {isLoggedIn ? (
+        <Link onClick={handleLogout} to="/logout">
+          <FaPowerOff size={24} />
+        </Link>
+      ) : (
+        <Link to="/login">
+          <FaSignInAlt size={24} />
+        </Link>
+      )}
+
+      {isLoggedIn && <FaCircle size={24} color="#66ff33" />}
     </Nav>
   );
 }
