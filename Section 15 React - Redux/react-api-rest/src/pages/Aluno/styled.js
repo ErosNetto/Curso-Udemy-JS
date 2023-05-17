42ac1212cee7ec58241cd1e60dde67dcf88a1bbd
Eros Netto <erosnetto1002@gmail.com>
2023-05-17 18:12:20 -0300

    Cadastro de alunos e edição

--- Section 15 React - Redux/react-api-rest/src/pages/Aluno/styled.js
+++ Section 15 React - Redux/react-api-rest/src/pages/Aluno/styled.js
@@ -1,3 +1,32 @@
 import styled from 'styled-components';
+import * as colors from '../../config/colors';
 
-export const Paragrafo = styled.p``;
+export const Form = styled.form`
+  margin-top: 20px;
+  display: flex;
+  flex-direction: column;
+
+  label {
+    display: flex;
+    flex-direction: column;
+    margin-bottom: 20px;
+  }
+
+  input {
+    height: 40px;
+    font-size: 18px;
+    border: 1px solid #ddd;
+    padding: 0 10px;
+    border-radius: 5px;
+    margin-top: 5px;
+    transition: all 300ms;
+
+    &:focus {
+      border: 1px solid ${colors.primaryColor};
+    }
+
+    &:hover {
+      border: 1px solid ${colors.primaryColor};
+    }
+  }
+`;
